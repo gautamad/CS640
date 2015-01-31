c8acbb5c67806afc849483fb1508b64ac24e0b8e
Gautam Dhanabalan <dhanabalan@macaroni-04.cs.wisc.edu>
2015-01-31 09:49:26 -0600

    Initial server code

--- Iperfer.java
+++ Iperfer.java
@@ -11,13 +11,13 @@ public class Iperfer {
 	    System.exit(0);
 	}
 	System.out.println("largs[0] = "+ args[0]);
-  	client = args[0].equals("-c");
-	server = args[0].equals("-s");
+  	client = args[0].equals("-c"); //Iperfer as Client
+	server = args[0].equals("-s"); //Iperfer as Server
 	cmdline[0] = args[0];
 	if(client) System.out.println("Success ");
 	if(client && argslength != 7) {
 	    System.out.println("Error: missing additional arguments");
-	    System.exit(0);
+	    ystem.exit(0);
 	}
 	else if(server && argslength !=3) {
 	    System.out.println("Error: missing additional arguments");
@@ -35,3 +35,24 @@ public class Iperfer {
 	}
     }
 }
+
+// Server Class
+
+public class IperferServer {
+    public void server(int serverPort) throws IOException {
+	ServerSocket serverSocket = null;
+	try {
+	    serverSocket = new ServerSocket(serverPort);
+	}
+	catch (IOException e) {
+	    System.err.println("Could not listen on port :" +serverPort);
+	}
+
+	Socket clientSocket = null;
+	System.out.println("Waiting for connection");
+	
+
+
+    }
+}
+
