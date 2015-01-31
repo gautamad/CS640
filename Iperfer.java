18994e340c04da3896a5b6204a26bc8401cde301
Gautam Dhanabalan <dhanabalan@macaroni-04.cs.wisc.edu>
2015-01-31 09:59:56 -0600

    Server code for printing recevied characters

--- Iperfer.java
+++ Iperfer.java
@@ -51,8 +51,35 @@ public class IperferServer {
 	Socket clientSocket = null;
 	System.out.println("Waiting for connection");
 	
-
+	try {
+	    clientSocket = serverSocket.accept();
+	}
+	catch (IOException e) {
+	    System.err.println("Cannot accept client");
+	    System.exit(1);
+	}
+	
+	System.out.println("Connection Successful");
+	
+	PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
+					  true); 
+	BufferedReader in = new BufferedReader (
+						new InputStreamReader( clientSocket.getInputStream())); 
+	
+	String inputLine; 
+	
+	while ((inputLine = in.readLine()) != null) { 
+	    System.out.println ("Server: " + inputLine); 
+	    out.println(inputLine); 
+	    
+	    if (inputLine.equals("Bye.")) 
+		break; 
+	} 
+	out.close(); 
+	in.close(); 
+	clientSocket.close(); 
+	serverSocket.close(); 
+   
 
     }
 }
-
